0f043cd270130350d12919bcdec2271d806fd877
Guillermo Alvarado <guillermoalvarado89@gmail.com>
2022-12-02 14:26:51 -0600

    chore:packer step without script

--- .circleci/scripts/build_ami.sh
+++ .circleci/scripts/build_ami.sh
@@ -2,5 +2,3 @@
 
 set -eu -o pipefail # Causes this script to terminate if any command returns an error
 
-packer build packer/ami.pkr.hcl
-echo "export NEW_AMI_ID=$(jq -r '.builds[0].artifact_id|split(":")[1]' ./manifest.json)" >> $BASH_ENV
