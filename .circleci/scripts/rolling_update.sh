ead64d51b85aa4ebcc58944a35d516b497b1e4ea
Guillermo Alvarado <guillermoalvarado89@gmail.com>
2022-12-02 15:49:11 -0600

    fix:NEW_IMAGE_ID

--- .circleci/scripts/rolling_update.sh
+++ .circleci/scripts/rolling_update.sh
@@ -8,4 +8,4 @@ echo $NEW_AMI_ID
 aws ec2 create-launch-template-version \
 --launch-template-name $LAUNCH_TEMPLATE_NAME \
 --source-version 1 \
---launch-template-data '{"ImageId":"'$NEW_IMAGE_ID'"}'
+--launch-template-data '{"ImageId":"'$NEW_AMI_ID'"}'
