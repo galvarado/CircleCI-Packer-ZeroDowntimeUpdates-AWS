62ce564524a3b6048b1c17da9a6dc67505cbe6ba
Guillermo Alvarado <guillermoalvarado89@gmail.com>
2022-12-02 14:48:15 -0600

    chore: env var

--- .circleci/scripts/rolling_update.sh
+++ .circleci/scripts/rolling_update.sh
@@ -5,4 +5,4 @@ set -eu -o pipefail # Causes this script to terminate if any command returns an 
 aws ec2 create-launch-template-version \
 --launch-template-name $LAUNCH_TEMPLATE_NAME \
 --source-version 1 \
---launch-template-data $NEW_AMI_ID
+--launch-template-data '{"ImageId":"$NEW_IMAGE_ID"}'
