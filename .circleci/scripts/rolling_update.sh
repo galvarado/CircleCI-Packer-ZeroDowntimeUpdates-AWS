b99a9ff3f0ef64f27fd7ddb281b69b48e3d01328
Guillermo Alvarado <guillermoalvarado89@gmail.com>
2022-12-02 15:15:33 -0600

    chore: print AMI_ID

--- .circleci/scripts/rolling_update.sh
+++ .circleci/scripts/rolling_update.sh
@@ -2,7 +2,10 @@
 
 set -eu -o pipefail # Causes this script to terminate if any command returns an error
 
+echo "AMI_ID to use..."
+echo $NEW_AMI_ID
+
 aws ec2 create-launch-template-version \
 --launch-template-name $LAUNCH_TEMPLATE_NAME \
 --source-version 1 \
---launch-template-data '{"ImageId":"$NEW_IMAGE_ID"}'
+--launch-template-data '{"ImageId":"'$NEW_IMAGE_ID'"}'
